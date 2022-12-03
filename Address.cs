6a3a5e9cf203fcbf57ef6b874c6dd2a152b1999b
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-03 19:38:47 +0300

    Add files via upload

---
+++ Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Address
+    {
+        public string ZipCode { get; set; }
+
+        void ValidateZipCode(string zipCode)
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ZipCode
+{
+    private readonly string value;
+    public ZipCode(string value)
+    {
+        this.value = value;
+    }
+    public string Value
+    {
+        get { return this.value; }   
+    }
+}
+
+public class Address
+{
+    public string ZipCode { get; set; }
+
+    void ValidateZipCode(string zipCode)
+    {
+
+    }
+}
+
